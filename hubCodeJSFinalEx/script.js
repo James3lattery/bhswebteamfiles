fda47431f352bb66ac5f1968563c923b6043b89c
James Slattery <james.slattery777@gmail.com>
2016-01-17 22:19:17 -0800

    Commit

--- hubCodeJSFinalEx/script.js
+++ hubCodeJSFinalEx/script.js
@@ -1,5 +1,32 @@
-$(document).ready(function() {
-    $('.navItem').click(function(){
-       alert("click"); 
+// Script By: Lighty_46
+$(document).ready(function () {
+    $(window).scroll(function () {
+
+        var y = $(this).scrollTop();
+
+        $('.link').each(function (event) {
+            if (y >= $($(this).attr('href')).offset().top - 40) {
+                $('.link').not(this).removeClass('active');
+                $(this).addClass('active');
+            }
+        });
+
+    });
+});
+
+// SMOOTH SCROLLING (with negative scroll of 40 for header)
+
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 40)
+                }, 850);
+                return false;
+            }
+        }
     });
 });
